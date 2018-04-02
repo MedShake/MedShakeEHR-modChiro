16a7cf5c4838dfb3ff5fe5645720863a7e0010ae
fr33z00 <frisou76@yahoo.fr>
2018-04-02 10:43:23 +0100

    Améliorations diverses

--- upgrade/chiro/sqlUpgrade_v3.0.1_v3.1.0_post.php
+++ upgrade/chiro/sqlUpgrade_v3.0.1_v3.1.0_post.php
@@ -1,14 +1,19 @@
 <?php
+$file_list=[
+    $p['homepath'].'controlers/module/chiro/patient/actions/inc-ajax-extractReglementForm.php',
+    $p['homepath'].'controlers/module/chiro/compta/actions/comptaActions.php',
+    $p['config']['templatesFolder'].'chiro/patient/patientReglementForm.html.twig',
+    $p['config']['templatesFolder'].'chiro/patient/inc-ajax-detReglement.html.twig',
+    $p['config']['templatesFolder'].'chiro/configuration/configActes.html.twig',
+    $p['config']['templatesFolder'].'chiro/configuration/configActesBase.html.twig',
+    $p['config']['templatesFolder'].'chiro/configuration/configActesCat.html.twig',
+    $p['config']['templatesFolder'].'chiro/compta/compta.html.twig',
+    $p['config']['templatesFolder'].'chiro/compta/comptaToday.html.twig',
+    $p['config']['templatesFolder'].'chiro/compta/comptaMemoTarifs.html.twig'
+];
 
-unlink($p['homepath'].'controlers/module/chiro/patient/actions/inc-ajax-extractReglementForm.php');
-unlink($p['homepath'].'controlers/module/chiro/patient/actions/inc-ajax-saveReglementForm.php');
-unlink($p['homepath'].'controlers/module/chiro/compta/actions/comptaActions.php');
-unlink($p['config']['templatesFolder'].'chiro/patient/patientReglementForm.html.twig');
-unlink($p['config']['templatesFolder'].'chiro/patient/inc-ajax-detReglement.html.twig');
-unlink($p['config']['templatesFolder'].'chiro/configuration/configActes.html.twig');
-unlink($p['config']['templatesFolder'].'chiro/configuration/configActesBase.html.twig');
-unlink($p['config']['templatesFolder'].'chiro/configuration/configActesCat.html.twig');
-unlink($p['config']['templatesFolder'].'chiro/compta/compta.html.twig');
-unlink($p['config']['templatesFolder'].'chiro/compta/comptaToday.html.twig');
-unlink($p['config']['templatesFolder'].'chiro/compta/comptaMemoTarifs.html.twig');
+foreach ($file_list as $file) {
+  if (is_file($file))
+    unlink($file);
+}
 
